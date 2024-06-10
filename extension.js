f89ac3e4ba1deaea1c939d1557a1f9b56c904737
ishkapoor2000 <ishkapoor2000@gmail.com>
2024-06-11 01:42:42 +0530

    Multiple API methods

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@ const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
+// @ts-ignore
 const acorn = require('acorn');
 const acornLoose = require('acorn-loose');
 const walk = require('acorn-walk');
@@ -10,142 +11,149 @@ const walk = require('acorn-walk');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    console.log('Extension "Chai Sutta Explorer" is now active!');
+	console.log('Extension "Chai Sutta Explorer" is now active!');
 
-    let disposable = vscode.commands.registerCommand('chai-sutta-explorer.showChaiSuttaSpots', function () {
-        console.log('Command "chai-sutta-explorer.showChaiSuttaSpots" executed');
+	let disposable = vscode.commands.registerCommand('chai-sutta-explorer.showChaiSuttaSpots', function () {
+		console.log('Command "chai-sutta-explorer.showChaiSuttaSpots" executed');
 
-        const panel = vscode.window.createWebviewPanel(
-            'chaiSuttaExplorer',
-            'Chai Sutta Explorer',
-            vscode.ViewColumn.One,
-            {
-                enableScripts: true // Enable scripts in the webview
-            }
-        );
+		const panel = vscode.window.createWebviewPanel(
+			'chaiSuttaExplorer',
+			'Chai Sutta Explorer',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true // Enable scripts in the webview
+			}
+		);
 
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-        if (workspaceFolders && workspaceFolders.length > 0) {
-            const folderPath = workspaceFolders[0].uri.fsPath;
-            console.log('Workspace folder path:', folderPath);
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (workspaceFolders && workspaceFolders.length > 0) {
+			const folderPath = workspaceFolders[0].uri.fsPath;
+			console.log('Workspace folder path:', folderPath);
 
-            try {
-                const apiCalls = findAPICalls(folderPath);
-                console.log('API Calls found:', apiCalls);
-                panel.webview.html = getWebviewContent(apiCalls);
-            } catch (error) {
-                console.error('Error finding API calls:', error);
-                panel.webview.html = getWebviewContent();
-            }
-        } else {
-            console.log('No workspace folder found');
-            panel.webview.html = getWebviewContent();
-        }
+			try {
+				const apiCalls = findAPICalls(folderPath);
+				console.log('API Calls found:', apiCalls);
+				panel.webview.html = getWebviewContent(apiCalls);
+			} catch (error) {
+				console.error('Error finding API calls:', error);
+				panel.webview.html = getWebviewContent();
+			}
+		} else {
+			console.log('No workspace folder found');
+			panel.webview.html = getWebviewContent();
+		}
 
-        panel.webview.onDidReceiveMessage(
-            async message => {
-                const fileUri = vscode.Uri.file(message.file);
-                const document = await vscode.workspace.openTextDocument(fileUri);
-                const editor = await vscode.window.showTextDocument(document);
-                const lineToGo = message.line - 1; // VS Code lines are 0-based
+		panel.webview.onDidReceiveMessage(
+			async message => {
+				const fileUri = vscode.Uri.file(message.file);
+				const document = await vscode.workspace.openTextDocument(fileUri);
+				const editor = await vscode.window.showTextDocument(document);
+				const lineToGo = message.line - 1; // VS Code lines are 0-based
 
-                editor.selections = [new vscode.Selection(lineToGo, 0, lineToGo, 0)];
-                editor.revealRange(new vscode.Range(lineToGo, 0, lineToGo, 0), vscode.TextEditorRevealType.InCenter);
-            },
-            undefined,
-            context.subscriptions
-        );
-    });
+				editor.selections = [new vscode.Selection(lineToGo, 0, lineToGo, 0)];
+				editor.revealRange(new vscode.Range(lineToGo, 0, lineToGo, 0), vscode.TextEditorRevealType.InCenter);
+			},
+			undefined,
+			context.subscriptions
+		);
+	});
 
-    context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
 
 function findAPICalls(dir) {
-    console.log('Scanning directory:', dir);
+	console.log('Scanning directory:', dir);
 
-    let apiCalls = [];
-    const configuration = vscode.workspace.getConfiguration('chaiSuttaExplorer');
-    const ignorePatterns = configuration.get('ignorePatterns');
+	let apiCalls = [];
+	const configuration = vscode.workspace.getConfiguration('chaiSuttaExplorer');
+	const ignorePatterns = configuration.get('ignorePatterns');
 
-    const jsFiles = glob.sync(`${dir}/**/*.{js,jsx,ts,tsx}`, { ignore: ignorePatterns });
-    const pyFiles = glob.sync(`${dir}/**/*.py`, { ignore: ignorePatterns });
+	const jsFiles = glob.sync(`${dir}/**/*.{js,jsx,ts,tsx}`, { ignore: ignorePatterns });
+	const pyFiles = glob.sync(`${dir}/**/*.py`, { ignore: ignorePatterns });
 
-    console.log('JavaScript/TypeScript files:', jsFiles);
-    console.log('Python files:', pyFiles);
+	console.log('JavaScript/TypeScript files:', jsFiles);
+	console.log('Python files:', pyFiles);
 
-    jsFiles.forEach(file => {
-        try {
-            const content = fs.readFileSync(file, 'utf8');
-            const calls = findJsApiCalls(content, file);
-            apiCalls = apiCalls.concat(calls);
-        } catch (error) {
-            console.error(`Error reading or parsing JS file ${file}:`, error);
-        }
-    });
+	jsFiles.forEach(file => {
+		try {
+			const content = fs.readFileSync(file, 'utf8');
+			const calls = findJsApiCalls(content, file);
+			apiCalls = apiCalls.concat(calls);
+		} catch (error) {
+			console.error(`Error reading or parsing JS file ${file}:`, error);
+		}
+	});
 
-    pyFiles.forEach(file => {
-        try {
-            const content = fs.readFileSync(file, 'utf8');
-            const calls = findPyApiCalls(content, file);
-            apiCalls = apiCalls.concat(calls);
-        } catch (error) {
-            console.error(`Error reading or parsing Python file ${file}:`, error);
-        }
-    });
+	pyFiles.forEach(file => {
+		try {
+			const content = fs.readFileSync(file, 'utf8');
+			const calls = findPyApiCalls(content, file);
+			apiCalls = apiCalls.concat(calls);
+		} catch (error) {
+			console.error(`Error reading or parsing Python file ${file}:`, error);
+		}
+	});
 
-    return apiCalls;
+	return apiCalls;
 }
 
 function findJsApiCalls(content, file) {
-    let apiCalls = [];
-    try {
-        const ast = acornLoose.parse(content, { ecmaVersion: 2020, sourceType: 'module', locations: true });
+	let apiCalls = [];
+	try {
+		const ast = acornLoose.parse(content, { ecmaVersion: 2020, sourceType: 'module', locations: true });
 
-        walk.simple(ast, {
-            CallExpression(node) {
-                if (node.callee.type === 'Identifier' && node.callee.name === 'fetch') {
-                    const methodNode = node.arguments[1] && node.arguments[1].properties.find(prop => prop.key.name === 'method');
-                    const method = methodNode ? methodNode.value.value : 'GET';
-                    apiCalls.push({ file, call: content.slice(node.start, node.end), line: node.loc.start.line, method });
-                } else if (node.callee.type === 'MemberExpression' && node.callee.object.name === 'axios') {
-                    const method = node.callee.property.name.toUpperCase();
-                    apiCalls.push({ file, call: content.slice(node.start, node.end), line: node.loc.start.line, method });
-                } else if (node.callee.type === 'Identifier' && node.callee.name === 'axios') {
-                    const methodNode = node.arguments[0] && node.arguments[0].properties.find(prop => prop.key.name === 'method');
-                    const method = methodNode ? methodNode.value.value.toUpperCase() : 'GET';
-                    apiCalls.push({ file, call: content.slice(node.start, node.end), line: node.loc.start.line, method });
-                }
-            },
-            NewExpression(node) {
-                if (node.callee.name === 'XMLHttpRequest') {
-                    walk.simple(node, {
-                        CallExpression(innerNode) {
-                            if (innerNode.callee.property && innerNode.callee.property.name === 'open') {
-                                const method = innerNode.arguments[0].value;
-                                apiCalls.push({ file, call: content.slice(innerNode.start, innerNode.end), line: innerNode.loc.start.line, method });
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    } catch (error) {
-        console.error(`Error parsing JS content in file ${file}:`, error);
-    }
-    return apiCalls;
+		walk.simple(ast, {
+			CallExpression(node) {
+				if (node.callee.type === 'Identifier' && node.callee.name === 'fetch') {
+					// @ts-ignore
+					const methodNode = node.arguments[1] && node.arguments[1].properties.find(prop => prop.key.name === 'method');
+					const method = methodNode ? methodNode.value.value : 'GET';
+					apiCalls.push({ file, call: content.slice(node.start, node.end), line: node.loc.start.line, method });
+				// @ts-ignore
+				} else if (node.callee.type === 'MemberExpression' && node.callee.object.name === 'axios') {
+					// @ts-ignore
+					const method = node.callee.property.name.toUpperCase();
+					apiCalls.push({ file, call: content.slice(node.start, node.end), line: node.loc.start.line, method });
+				} else if (node.callee.type === 'Identifier' && node.callee.name === 'axios') {
+					// @ts-ignore
+					const methodNode = node.arguments[0] && node.arguments[0].properties.find(prop => prop.key.name === 'method');
+					const method = methodNode ? methodNode.value.value.toUpperCase() : 'GET';
+					apiCalls.push({ file, call: content.slice(node.start, node.end), line: node.loc.start.line, method });
+				}
+			},
+			NewExpression(node) {
+				// @ts-ignore
+				if (node.callee.name === 'XMLHttpRequest') {
+					walk.simple(node, {
+						CallExpression(innerNode) {
+							// @ts-ignore
+							if (innerNode.callee.property && innerNode.callee.property.name === 'open') {
+								// @ts-ignore
+								const method = innerNode.arguments[0].value;
+								apiCalls.push({ file, call: content.slice(innerNode.start, innerNode.end), line: innerNode.loc.start.line, method });
+							}
+						}
+					});
+				}
+			}
+		});
+	} catch (error) {
+		console.error(`Error parsing JS content in file ${file}:`, error);
+	}
+	return apiCalls;
 }
 
 function findPyApiCalls(content, file) {
-    let apiCalls = [];
-    const lines = content.split('\n');
-    const regex = /requests\.(get|post|put|delete|patch|options|head)\(([^)]+)\)/g;
-    lines.forEach((line, index) => {
-        let match;
-        while ((match = regex.exec(line)) !== null) {
-            apiCalls.push({ file, call: match[0], line: index + 1, method: match[1].toUpperCase() });
-        }
-    });
-    return apiCalls;
+	let apiCalls = [];
+	const lines = content.split('\n');
+	const regex = /requests\.(get|post|put|delete|patch|options|head)\(([^)]+)\)/g;
+	lines.forEach((line, index) => {
+		let match;
+		while ((match = regex.exec(line)) !== null) {
+			apiCalls.push({ file, call: match[0], line: index + 1, method: match[1].toUpperCase() });
+		}
+	});
+	return apiCalls;
 }
 
 /**
@@ -156,22 +164,61 @@ function findPyApiCalls(content, file) {
  * list of API calls. Each API call is displayed as a list item with the file name and the call itself.
  */
 function getWebviewContent(apiCalls = []) {
-    let content = `
+	const methodInfo = {
+        'GET': {
+            name: 'Chai☕',
+            calls: []
+        },
+        'POST': {
+            name: 'Sutta🚬',
+            calls: []
+        },
+        'PUT': {
+            name: 'Chai Latte🍼',
+            calls: []
+        },
+        'DELETE': {
+            name: 'Cold Coffee🍵',
+            calls: []
+        },
+        'PATCH': {
+            name: 'Weed🚭',
+            calls: []
+        },
+        'UPDATE': {
+            name: 'Green Tea🥗',
+            calls: []
+        }
+    };
+
+	// Group API calls by method
+	apiCalls.forEach(apiCall => {
+		methodInfo[apiCall.method].calls.push(apiCall);
+	});
+
+	// Generate HTML content for each method section
+	let content = `
         <h1>Chai Sutta Spots</h1>
-        <h2>Chai - GET</h2>
-		<ul>
-			${apiCalls.filter(call => call.method === 'GET').map(apiCall => {
+    `;
+
+	for (const method in methodInfo) {
+		const { name, calls } = methodInfo[method];
+		if (calls.length > 0) {
+
+			content += `
+                <h2>${name} - ${method}</h2>
+                <ul>
+                    ${calls.map(apiCall => {
 				const link = `command:chai-sutta-explorer.openFileAtLine?${encodeURIComponent(JSON.stringify({ file: apiCall.file, line: apiCall.line }))}`;
 				return `<li><a title="${apiCall.file}" href="${link}">${path.basename(apiCall.file)}:${apiCall.line}</a> ${apiCall.call}</li>`;
 			}).join('')}
-		</ul>
-		<h2>Sutta - POST</h2>
-		<ul>
-			${apiCalls.filter(call => call.method === 'POST').map(apiCall => {
-				const link = `command:chai-sutta-explorer.openFileAtLine?${encodeURIComponent(JSON.stringify({ file: apiCall.file, line: apiCall.line }))}`;
-				return `<li><a title="${apiCall.file}" href="${link}">${path.basename(apiCall.file)}:${apiCall.line}</a> ${apiCall.call}</li>`;
-			}).join('')}
-		</ul>
+                </ul>
+            `;
+		}
+	}
+
+	// Add script for VS Code communication
+	content += `
         <script>
             const vscode = acquireVsCodeApi();
             document.querySelectorAll('a').forEach(link => {
@@ -184,15 +231,17 @@ function getWebviewContent(apiCalls = []) {
             });
         </script>
     `;
-    return content;
+
+	return content;
 }
+
 
 // This method is called when your extension is deactivated
 function deactivate() {
-    console.log('Extension "Chai Sutta Explorer" is now deactivated');
+	console.log('Extension "Chai Sutta Explorer" is now deactivated');
 }
 
 module.exports = {
-    activate,
-    deactivate
+	activate,
+	deactivate
 }
